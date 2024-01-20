a60655cc85c25537c53ac903963db11282b00051
twkhjl <twkhjl@gmail.com>
2024-01-20 12:10:14 +0800

    install chakraUI;clear index.css;add testing components in App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -5,6 +7,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
