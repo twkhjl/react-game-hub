a5576291285c149d2c76d1aae6ca5d2dce88774b
twkhjl <twkhjl@gmail.com>
2024-02-13 22:33:43 +0800

    add no-image placeholder image

--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,6 +1,10 @@
 
+import noImage from "../assets/no-image-placeholder.webp";
+
 
 const getCroppedImageUrl = function(url:string){
+
+    if(!url) return noImage;
 
     return url.replace(/(\/media\/)/,"$1"+"crop/600/400/");
 }
