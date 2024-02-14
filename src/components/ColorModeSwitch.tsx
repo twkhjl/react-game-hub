a0614845ab748ce000b693a7d94c5d33434ac891
twkhjl <twkhjl@gmail.com>
2024-02-14 11:04:27 +0800

    modify color mode switch for rwd ux

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,16 +1,23 @@
-import { HStack, useColorMode } from "@chakra-ui/react";
+import { HStack, useBreakpointValue, useColorMode } from "@chakra-ui/react";
 import { Switch } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
 import { FaRegSun, FaMoon } from "react-icons/fa6";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
   return (
-    <HStack>
-      {colorMode === "light" ? "" : <Icon as={FaMoon} boxSize={6} />}
-      <Switch onChange={toggleColorMode} colorScheme="green" size="lg" />
-      {colorMode === "dark" ? "" : <Icon as={FaRegSun} boxSize={6} />}
-    </HStack>
+    <>
+      <HStack marginRight={0} marginLeft={"auto"}>
+        {colorMode === "dark" && isLargeScreen && (
+          <Icon as={FaMoon} boxSize={6} />
+        )}
+        <Switch onChange={toggleColorMode} colorScheme="green" size="lg" />
+        {colorMode === "light" && isLargeScreen && (
+          <Icon as={FaRegSun} boxSize={6} />
+        )}
+      </HStack>
+    </>
   );
 };
 
